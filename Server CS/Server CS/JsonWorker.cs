637f9796bca336ab2a153cbd281631814d1bf663
mirpro2001 <usov-miroslav@mail.ru>
2020-11-19 13:37:01 +0300

    Commented some methods.

--- Server CS/Server CS/JsonWorker.cs
+++ Server CS/Server CS/JsonWorker.cs
@@ -9,25 +9,26 @@ namespace Server_CS
     internal class JsonWorker
     {
         /// <summary>
-        ///     Файл для сохранения сообщений в виде json массива
+        ///     Файл для сохранения сообщений и регистрационных данных (логины и пароли) в виде json массива.
         /// </summary>
         private const string MessagesPath = @"messages.json";
-
         private const string RegDataPath = @"regData.json";
 
         /// <summary>
-        ///     Функция сохранения в файл
+        ///     Метот класса, который сохраняет сообщения в файл.
         /// </summary>
         /// <param name="messages">Массив сообщений</param>
         public static async void Save(List<Message> messages)
         {
             var NumberOfRetries = 3;
             var DelayOnRetry = 1000;
-
             for (var i = 1; i <= NumberOfRetries; ++i)
                 try
                 {
+                    ///await запускает асинхронную задачу инициализации новый экземпляр класса StreamWriter для указанного файла 
+                    ///с помощью кодировки по умолчанию и размера буфера.
                     await using var streamWriter = new StreamWriter(MessagesPath);
+                    ///await запускает асинхронную задачу WriteAsync.
                     await streamWriter.WriteAsync(JsonConvert.SerializeObject(messages));
                     break;
                 }
@@ -36,19 +37,17 @@ namespace Server_CS
                     Thread.Sleep(DelayOnRetry);
                 }
         }
-
         /// <summary>
-        ///     Функция сохранения в файл
+        ///     Функция сохранения регистрационных данных в файл.
         /// </summary>
-        /// <param name="regDatas">массив связок логин/пароль</param>
+        /// <param name="regDatas">Массив связок логин/пароль.</param>
         public static async void Save(List<RegData> regDatas)
         {
             await using var streamWriter = new StreamWriter(RegDataPath);
             await streamWriter.WriteAsync(JsonConvert.SerializeObject(regDatas));
         }
-
         /// <summary>
-        ///     Функция загрузки сообщений из файла, десирилизованный из json в List <Message> объект
+        ///     Функция загрузки сообщений из файла, десериализованных из json в List <Message> объект.
         /// </summary>
         public static async void Load()
         {
@@ -62,7 +61,6 @@ namespace Server_CS
             catch (Exception)
             {
             }
-
             if (!File.Exists(RegDataPath))
                 return;
             try
